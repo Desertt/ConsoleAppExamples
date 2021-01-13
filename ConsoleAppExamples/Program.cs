cd648116cbeba9b473a94e980508dc89c6a98fe1
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2021-01-14 02:37:45 +0300

    charwork

--- ConsoleAppExamples/Program.cs
+++ ConsoleAppExamples/Program.cs
@@ -374,12 +374,35 @@ namespace ConsoleAppExamples
 
             #endregion
 
-            #region Klavyeden girilen metnin kaç kelimeden oluştuğunu bulan Console Application örneği:
-
-            #endregion
+            #region  Girilen string ifadede “,” karakterlerini “.” karakteri olarak değiştiren programı yazınız.
+            String metin = "www.yazilimkodlama.com";
+            metin = metin.ToLower();
+            String karakterler = "0123456789abcdefghijklmnopqrstuvwxyz.?,;";
+            int[] count = new int[karakterler.Length];
+            for (int i = 0; i < metin.Length; i++)
+            {
+                int index = karakterler.IndexOf(metin[i]);
+                if (index < 0)
+                    continue;
+                else
+                {
+                    count[index]++;
+                }
+            }
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] < 1)
+                    continue;
+                else
+                {
+                    Console.WriteLine(karakterler[i] + " " + count[i]);
+                }
+            }
+            Console.ReadKey();
 
 
         }
     }
 }
 
+#endregion
