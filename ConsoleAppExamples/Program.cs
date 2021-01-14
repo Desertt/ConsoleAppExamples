5e6d302cfcbb3876a4dd14d3a778adc133659db1
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2021-01-14 23:47:58 +0300

    Examples

--- ConsoleAppExamples/Program.cs
+++ ConsoleAppExamples/Program.cs
@@ -375,34 +375,27 @@ namespace ConsoleAppExamples
             #endregion
 
             #region  Girilen string ifadede “,” karakterlerini “.” karakteri olarak değiştiren programı yazınız.
-            String metin = "www.yazilimkodlama.com";
-            metin = metin.ToLower();
-            String karakterler = "0123456789abcdefghijklmnopqrstuvwxyz.?,;";
-            int[] count = new int[karakterler.Length];
-            for (int i = 0; i < metin.Length; i++)
-            {
-                int index = karakterler.IndexOf(metin[i]);
-                if (index < 0)
-                    continue;
-                else
-                {
-                    count[index]++;
-                }
-            }
-            for (int i = 0; i < count.Length; i++)
-            {
-                if (count[i] < 1)
-                    continue;
-                else
-                {
-                    Console.WriteLine(karakterler[i] + " " + count[i]);
-                }
-            }
+            /* string metin = "";
+             Console.Write("Metin Giriniz : ");
+             metin = Console.ReadLine();
+             string newMetin = metin.Replace(',', '.');
+             Console.WriteLine(newMetin);
+             Console.ReadKey();
+            */
+
+
+            #endregion
+
+            #region Girilen String ifadede boşluk karakterine kadar olan kısmı yazdıran
+            string ifade;
+            Console.Write("Metin Giriniz : ");
+            ifade = Console.ReadLine();
+            int no = ifade.IndexOf(' ');
+            Console.WriteLine(ifade.Substring(0, no));
             Console.ReadKey();
 
+            #endregion
 
         }
     }
 }
-
-#endregion
